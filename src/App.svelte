3fb03b7eecad8188e153769368573258ff4de0d0
Sasha Pylypenko <oleksandr.pylypenko@techmagic.co>
2021-12-30 16:55:12 +0200

    feat: playdeck and one component

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,63 @@
 <script>
-	export let name;
-	let author = 'Sasha Pylypenko!'
+	import Square from './components/Square.svelte'
+
+	export const author = 'Sasha Pylypenko!';
+	 let name = prompt('Enter your name please');
+
+
+	 const size = 5;
+	 let data = makeField(size)
+	 function makeField(size) {
+		let body = [];
+
+		for (let i = 1; i <= size; i++) {
+			body.push([]);
+			for (let j = 1; j <= size; j++) {
+			body[i - 1].push({ row: i, col: j });
+			}
+		}
+		return body;
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<h2>The author is {author}</h2>
-	<img src="https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80" alt=""/>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<section>
+		<h1>Hello {name}!</h1>
+		<h2>The author is {author}</h2>
+		<img src="https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80" alt=""/>
+		<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	</section>
+	<section class="wrapper">
+		{#each data as item, idx}
+		<div key={idx}>
+			{#each item as i, idx}
+			<Square
+				key={idx}
+				size={size}
+				data-col={i.col}
+				data-row={i.row}
+				text={i.col}
+				>
+			</Square>
+			{/each}
+		</div>
+		{/each}
+	</section>
 </main>
 
 <style>
-	main {
+	.section {
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
+		height: 100vh;
+	}
+
+	.wrapper{
+		display: flex;
+		max-width: max-content;
+		margin: 20px auto;
 	}
 
 	h1 {
@@ -37,7 +79,7 @@
 	}
 
 	@media (min-width: 640px) {
-		main {
+		.section {
 			max-width: none;
 		}
 	}
---
+++ src/components/Square.svelte
@@ -0,0 +1,18 @@
+<script>
+    let text;
+</script>
+
+<div>{text}</div>
+
+<style>
+    div{
+  height: 40px;
+  border: 1px solid black;
+  cursor: pointer;
+  transition: 0 background-color;
+  }
+    div:hover {
+    transition-delay: 0.08s;
+    background-color: lightblue;
+  }
+</style>
